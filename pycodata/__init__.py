206ae5caba65780fa8cdd06cb741ee28f0e541dc
Milan Skocic <milan.skocic@icloud.com>
2023-03-03 13:08:24 +0100

    BUG: Fix path to Intel fortran redistributable for windows.

--- pycodata/__init__.py
+++ pycodata/__init__.py
@@ -3,8 +3,8 @@ import platform
 import os
 import pathlib
 
-intel_redist_32 = pathlib.Path("C:\\Program Files (x86)\\Common Files\\Intel\\Shared Libraries\\redist\\ia32\\compiler")
-intel_redist_64 = pathlib.Path("C:\\Program Files (x86)\\Common Files\\Intel\\Shared Libraries\\redist\\win_64\\compiler")
+intel_redist_32 = pathlib.Path("C:\\Program Files (x86)\\Common Files\\Intel\\Shared Libraries\\redist\\ia32_win\\compiler")
+intel_redist_64 = pathlib.Path("C:\\Program Files (x86)\\Common Files\\Intel\\Shared Libraries\\redist\\intel64_win\\compiler")
 arch = None
 
 if platform.system() == "Windows":
@@ -13,12 +13,12 @@ if platform.system() == "Windows":
         if intel_redist_64.exists():
             os.add_dll_directory(intel_redist_64)
         else:
-            print("ERROR: Intel Fortran Redistributable win_64 was not found.")
+            print("ERROR: Intel Fortran Redistributable intel64_win was not found.")
     elif arch == "32bit":
         if intel_redist_32.exists():
             os.add_dll_directory(intel_redist_32)
         else:
-            print("ERROR: Intel Fortran Redistributable ia32 was not found.")
+            print("ERROR: Intel Fortran Redistributable ia32_win was not found.")
     else:
         print(f"Error: Architecture was not properly detected:  arch={arch:s}")
         print("Error: dll dependencies cannot be loaded.")
