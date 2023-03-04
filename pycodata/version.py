302aaf6eeb8255cfeed6e7c192cb30ae63176bf8
Milan Skocic <milan.skocic@icloud.com>
2023-03-04 07:01:08 +0100

    Update version.py
    Change copyright.

--- pycodata/version.py
+++ pycodata/version.py
@@ -16,5 +16,5 @@ __author__ = "Milan Skocic"
 __author_email__ = "milan.skocic@icloud.com"
 __maintainer__ = __author__
 __maintainer_email__ = __author_email__
-__copyright__ = 'Copyright (C) 2022-2023 ' + __author__
+__copyright__ = 'Copyright (C) 2023 ' + __author__
 __license__ = 'GNU General Public License v3 (GPLv3)'
